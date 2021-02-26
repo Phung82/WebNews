2cf05e876cd9251a0b583d1000427494ff61936f
Phun82 <pn8778773@gmail.com>
2021-02-26 14:57:21 +0700

    add home 1 - staticfiles

--- sportnews/home/tests.py
+++ sportnews/home/tests.py
@@ -1,3 +1,8 @@
-from django.test import TestCase
+#from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase, SimpleTestCase
+class SimpleTest(SimpleTestCase):
+    def test_home_page_status(self):
+        respone = self.client.get('/')
+        self.assertEquals(respone.status_code,200)
--- sportnews/sportnews/settings.py
+++ sportnews/sportnews/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home', 
 ]
 
 MIDDLEWARE = [
@@ -63,6 +64,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+        'libraries': {  # Adding this section should work around the issue.
+            'staticfiles': 'django.templatetags.static',
+        },
         },
     },
 ]
