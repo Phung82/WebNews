e403c48135aa573a6ee51a50db67d4194e630d35
Phun82 <pn8778773@gmail.com>
2021-02-26 16:22:35 +0700

    upadate

--- sportnews/home/urls.py
+++ sportnews/home/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 #urlpatterns laf 1 LIST luu cac duong dan
 urlpatterns = [
-    path('',views.index),
+    path('', views.index),
+    path('contact/', views.v_contact, name='contacttt'),
+    path('about/', views.v_about,name="abouttt"),
+    path('post/', views.v_post,name="posttt"),
+
 ]
--- sportnews/home/views.py
+++ sportnews/home/views.py
@@ -2,10 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 
-
-
 from django.http import HttpResponse
-
 
 def index(request):
     return render(request,"pages/home.html")
+
+def v_contact(request):
+    return render(request,"pages/contact.html")
+
+def v_about(request):
+    return render(request,"pages/about.html")
+
+def v_post(request):
+    return render(request,"pages/post.html")
+
+
