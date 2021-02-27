97aaf3a52399522f28f3a68153288e2fe89d7e7f
Phun82 <pn8778773@gmail.com>
2021-02-27 13:41:28 +0700

    add app blog

---
+++ sportnews/blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+"""
+c_Post dung de luu cac bai biet thuoc lop con models.Model
+
+"""
+class c_Post(models.Model):
+    title = models.CharField(max_length = 100)
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+     
--- sportnews/home/urls.py
+++ sportnews/home/urls.py
@@ -4,8 +4,8 @@ from . import views
 #urlpatterns laf 1 LIST luu cac duong dan
 urlpatterns = [
     path('', views.index),
-    path('contact/', views.v_contact, name='contacttt'),
-    path('about/', views.v_about,name="abouttt"),
-    path('post/', views.v_post,name="posttt"),
+    path('contact', views.v_contact, name='contacttt'),
+    path('about', views.v_about,name="abouttt"),
+    path('post', views.v_post,name="posttt"),
 
 ]
